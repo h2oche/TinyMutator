08f5bc289ae334cd7744a9685635dc4ec4c34f1e
Changhun Kim <ssohot1@kaist.ac.kr>
2020-06-03 01:33:45 +0900

    before full

--- src/mutate.rs
+++ src/mutate.rs
@@ -1,15 +1,28 @@
 // use quote::quote;
-// use syn::{parse_macro_input, DeriveInput};
+extern crate proc_macro;
+extern crate syn;
+use syn::{parse_macro_input, DeriveInput, Type, Expr, Result, parse_str, parse};
 use std::fs::File;
 use std::io::prelude::*;
 
-pub fn mutate(filename: String, line: i32) {
+pub fn mutate(filename: String, line: i32) -> Result<()> {
     println!("filename : {}", filename);
     println!("line : {}", line);
   
     let mut f = File::open(filename).expect("file not found");
     let mut contents = String::new();
     f.read_to_string(&mut contents).expect("Something went wrong reading the file");
-    println!("With text:\n{}", contents);
-    println!("{}", contents.chars().nth(line).unwrap());
+    // println!("With text:\n{}", contents);
+
+    // let code = "assert_eq!(u8::max_value(), 255)";
+    let expr = syn::parse_str::<Expr>(&contents)?;
+    println!("{:#?}", expr);
+
+    // for line in expr {
+    //     println!("{}", line)
+    // }
+    Ok(())
+    // println!("{}", contents.chars().nth(line).unwrap());
+    // let ast: Type = syn::parse_str(&contents).unwrap();
+    // let stream: proc_macro::TokenStream = contents.parse().unwrap();
 }
