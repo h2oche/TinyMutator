52c22d200488b216ece51e281b0cd570eee605c2
KimJungwoo <jw950310@kaist.ac.kr>
2020-06-03 00:07:05 +0900

    Start to implement : Reading file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use std::env;
+use std::fs;
+fn print_file() {
+    let args: Vec<String> = env::args().collect();
+    
+    let filename = &args[1];
+    let contents = fs::read_to_string(filename)
+        .expect("Something went wrong reading the file");
+
+    println!("With text:\n{}", contents);
+}
+
 fn main() {
     println!("Hello, world!");
+    print_file(); // cargo run ./src/guessing_game.rs
 }
