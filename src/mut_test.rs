019b62655f78adbf3b16dec85606d3ce29c6d10d
kms040411 <kms040411@kaist.ac.kr>
2020-06-09 21:28:09 +0900

    mutation testing debuged

--- src/mut_test.rs
+++ src/mut_test.rs
@@ -1,12 +1,11 @@
 //use std::io;
-#[path = "./utils.rs"]
-mod utils;
-#[path = "./mut_gen.rs"]
-mod mut_gen;
+use super::utils;
+use super::mut_gen::MutantInfo;
 use std::process::Command;
 use std::str;
-use std::fs::File;
+use std::fs::{File, OpenOptions};
 use std::io::prelude::*;
+use std::fmt;
 
 pub enum TestResult {
     Survived,
@@ -14,21 +13,41 @@ pub enum TestResult {
     CompileError,
 }
 
+impl fmt::Display for TestResult {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match *self {
+            TestResult::Survived => write!(f, "Survived"),
+            TestResult::Killed => write!(f, "Killed"),
+            TestResult::CompileError => write!(f, "CompileError"),
+        }
+    }
+}
+
 /**
  * Run tests and check whether mutants are killed.
  * Return lists of mutants and its results.
  */
-pub fn mut_test(path: String, list_of_mutants: Vec<&mut_gen::MutantInfo>) -> Vec<(String, TestResult)> {
-    let mutants_iter = list_of_mutants.iter();
+pub fn mut_test(path: String, list_of_mutants: Vec<MutantInfo>) -> Vec<(String, TestResult)> {
     let mut result : Vec<(String, TestResult)> = Vec::new();
+    if list_of_mutants.len() == 0 {
+        return result;
+    }
+    let mutants_iter = list_of_mutants.iter();
     let original_test_result = run_mut_test(&path, None).unwrap();
+    /*let original_test_result2 = run_mut_test(&path, None).unwrap();
+    if check_survive(&original_test_result2, &original_test_result){
+        println!("survive");
+    } else {
+        println!("killed");
+    }
+    return result;*/
     for mutant in mutants_iter {
-        let original_source_code = replace_source(mutant);
+        let original_source_code = replace_source(&path, mutant);
         let mut_test_result = match run_mut_test(&path, None) {
             Some(v) => v,
             None => {
                 result.push((mutant.file_name.clone(), TestResult::CompileError));
-                restore_source(original_source_code);
+                restore_source(&path, original_source_code);
                 continue;
             }
         };
@@ -37,7 +56,7 @@ pub fn mut_test(path: String, list_of_mutants: Vec<&mut_gen::MutantInfo>) -> Vec
         } else {
             result.push((mutant.file_name.clone(), TestResult::Killed));
         }
-        restore_source(original_source_code);
+        restore_source(&path, original_source_code);
     }
     return result;
 }
@@ -46,35 +65,37 @@ pub fn mut_test(path: String, list_of_mutants: Vec<&mut_gen::MutantInfo>) -> Vec
  * Replace source file to mutated source file
  * Return original original source file (source_code, file_name)
  */
-fn replace_source(mutate: &mut_gen::MutantInfo) -> (String, &String) {
-    let current_dir = utils::get_cwd();
+fn replace_source(path: &String, mutate: &MutantInfo) -> (String, String) {
     let file_name : &String = &mutate.file_name;
+    let file_name : String = String::new() + &utils::get_cwd() + "/" + file_name;
     let original_file_name = &mutate.source_name;
 
     // Read original source code and save it.
-    let mut f = File::open(String::new() + &current_dir + "/src/" + original_file_name).expect("File Not Found");
+    let mut f = OpenOptions::new().read(true).open(original_file_name).expect("File Not Found");
     let mut source_code = String::new();
     f.read_to_string(&mut source_code).expect("Failed to Read File");
 
     // Read mutated source code and write it to the original file.
-    let mut m = File::open(String::new() + &current_dir + "/src/" + file_name).expect("File Not Found");
+    let mut m = OpenOptions::new().write(true).read(true).open(file_name).expect("File Not Found");
     let mut mutated_code = String::new();
     m.read_to_string(&mut mutated_code).expect("Failed to Read File");
+
+    let mut f = OpenOptions::new().write(true).truncate(true).read(true).open(original_file_name).expect("File Not Found");
     f.write(mutated_code.as_bytes()).expect("Failed to Write File");
 
-    return (source_code, original_file_name);
+    return (source_code, original_file_name.clone());
 }
 
 /**
  * Restore original source file
  */
-fn restore_source(original_source_code: (String, &String)) {
-    let current_dir = utils::get_cwd();
+fn restore_source(path: &String, original_source_code: (String, String)) {
     let source_code = original_source_code.0;
     let file_name = original_source_code.1;
 
     // Open original file and Write original source code.
-    let mut f = File::open(current_dir + "/src/" + file_name).expect("File Not Found");
+    let mut f = OpenOptions::new().write(true).truncate(true).read(true).open(file_name).expect("File Not Found");
+    //println!("{}", source_code);
     f.write(source_code.as_bytes()).expect("Failed to Write File");
     return;
 }
@@ -85,11 +106,22 @@ fn restore_source(original_source_code: (String, &String)) {
  */
 pub fn check_survive(mut_test_result: &Vec<(String, bool)>, origin_test_result: &Vec<(String, bool)>) -> bool {
     // @TODO: This function is not tested(May have some bugs)
-    if mut_test_result == origin_test_result{
-        // @TODO: Check equivalence mutant
-        return true;
+    if mut_test_result.len() != origin_test_result.len(){
+        //println!("length differ");
+        return false;
     }
-    return false;
+    for i in 0..mut_test_result.len(){
+        if mut_test_result[i].0 != origin_test_result[i].0 {
+            //println!("String differ {}", i);
+            return false;
+        }
+        if mut_test_result[i].1 != origin_test_result[i].1 {
+            //println!("Result differ {}", i);
+            return false;
+        } 
+    }
+    
+    return true;
 }
 
 /**
@@ -116,11 +148,16 @@ pub fn run_mut_test(path: &String, tests: Option<Vec<String>>) -> Option<Vec<(St
     };
     if !output.status.success() {
         // Compile failed
-        return None;
+        if output.status.code().unwrap() != 101 {
+            println!("{}", output.status);
+            return None;
+        }
     }
     let output = String::from_utf8(output.stdout).unwrap();
+    //println!("======================");
+    //println!("{}", output);
+    //println!("====================");
     let parsed = parse_result(output).unwrap();
-    //println!("{:?}", parsed);
     return Some(parsed);
 }
 
@@ -151,5 +188,6 @@ pub fn parse_result(result: String) -> Option<Vec<(String, bool)>>{
             
         }
     }
+    result.sort_by(|a, b| b.0.cmp(&a.0));
     return Some(result);
 }
